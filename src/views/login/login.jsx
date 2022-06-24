3fadefab2a3a4b77fdb5936605352c7625c6d6db
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2022-06-24 00:06:15 -0300

    1° versão do login finalizado
    - Página Login
    - Input Grande
    - Titulo Grande
    - Botão Grande

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,9 +1,25 @@
+import React from 'react'
+
+import TituloGrande from '../../components/titulo-grande/tituloGrande'
+import InputGrande from '../../components/input-grande/inputGrande'
+import BotaoGrande from '../../components/botao-grande/botaoGrande'
 
 import './login.css';
 
 function App() {
   return (
-    <div >
+    <div className='entire-page-login'>
+      <section className='title-section-login'>
+        <TituloGrande title="Bahia Mar" />
+      </section>
+      <section className="caixa-central-section-login">
+        <InputGrande label="E-mail" type="text" />
+        <InputGrande label="Senha" type="password" />
+        <BotaoGrande text="Entrar" />
+        <a className="texto-redefinir-senha-login" href="/redefinir-senha">
+          Esqueceu sua senha? Clique aqui para recuperá-la
+        </a>
+      </section>
     </div>
   );
 }
